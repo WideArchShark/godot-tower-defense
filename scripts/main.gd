fe77f490d3b951f38b7e604362dd74aef524f83e
WideArchShark <widearchshark@gmail.com>
2023-09-28 22:06:36 +0100

    We got loops!

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,10 +4,16 @@ extends Node3D
 @export var tile_end:PackedScene
 @export var tile_straight:PackedScene
 @export var tile_corner:PackedScene
+@export var tile_crossroads:PackedScene
 @export var tile_empty:PackedScene
 
 @export var map_length:int = 16
 @export var map_height:int = 9
+
+@export var min_path_size = 50
+@export var max_path_size = 70
+@export var min_loops = 3
+@export var max_loops = 5
 
 var _pg:PathGenerator
 
@@ -16,6 +22,16 @@ func _ready():
 	_pg = PathGenerator.new(map_length, map_height)
 	_display_path()
 	_complete_grid()
+	
+	await get_tree().create_timer(2).timeout
+	_pop_along_grid()
+	
+func _pop_along_grid():
+	var box = CSGBox3D.new()
+	add_child(box)
+	for element in _pg.get_path():
+		box.global_position = Vector3(element.x,0.1,element.y)
+		await get_tree().create_timer(0.25).timeout
 	
 func _complete_grid():
 	for x in range(map_length):
@@ -26,19 +42,26 @@ func _complete_grid():
 				tile.global_position = Vector3(x, 0, y)
 	
 func _display_path():
-	var _path:Array[Vector2i] = _pg.generate_path()
-	
-	while _path.size() < 30:
-		_path = _pg.generate_path()
+	var iteration_count:int = 1
+	_pg.generate_path(true)
 
-	for element in _path:
-		var tile_score:int = _pg.get_tile_score(element)
+	while _pg.get_path().size() < min_path_size or _pg.get_path().size() > max_path_size or _pg.get_loop_count() < min_loops or _pg.get_loop_count() > max_loops:
+		iteration_count += 1
+#		print(iteration_count, " ", _pg.get_loop_count())
+		_pg.generate_path(true)
+
+	print("Generated a path of %d tiles after %d iterations" % [_pg.get_path().size(), iteration_count])
+	print(_pg.get_path())
+	
+	
+	for i in range(_pg.get_path().size()):
+		var tile_score:int = _pg.get_tile_score(i)
 		
 		var tile:Node3D = tile_empty.instantiate()
 		var tile_rotation: Vector3 = Vector3.ZERO
 		
 		if tile_score == 2:
-			tile = tile_start.instantiate()
+			tile = tile_end.instantiate()
 			tile_rotation = Vector3(0,-90,0)
 		elif tile_score == 8:
 			tile = tile_start.instantiate()
@@ -61,9 +84,12 @@ func _display_path():
 		elif tile_score == 3:
 			tile = tile_corner.instantiate()
 			tile_rotation = Vector3(0,270,0)
+		elif tile_score == 15:
+			tile = tile_crossroads.instantiate()
+			tile_rotation = Vector3(0,0,0)
 			
 		add_child(tile)
-		tile.global_position = Vector3(element.x, 0, element.y)
+		tile.global_position = Vector3(_pg.get_path_tile(i).x, 0, _pg.get_path_tile(i).y)
 		tile.global_rotation_degrees = tile_rotation
 	
 
--- scripts/path_generator.gd
+++ scripts/path_generator.gd
@@ -3,6 +3,7 @@ class_name PathGenerator
 
 var _grid_length:int
 var _grid_height:int
+var _loop_count:int
 
 var _path: Array[Vector2i]
 
@@ -10,8 +11,9 @@ func _init(length:int, height:int):
 	_grid_length = length
 	_grid_height = height
 
-func generate_path():
+func generate_path(add_loops:bool = false):
 	_path.clear()
+	_loop_count = 0
 	randomize()
 	
 	var x = 0
@@ -29,13 +31,26 @@ func generate_path():
 			y += 1
 		elif choice == 2 and y > 1 and not _path.has(Vector2i(x,y-1)):
 			y -= 1
+	
+	if add_loops:
+		# Running add_loops multiple times, as I think the first time it might miss an opportunity.
+		# Haven't quite figured out why yet, but adding it twice adds more loops!
+		_add_loops()
+		_add_loops()
+		_add_loops()
 		
 	return _path
 
-func get_tile_score(tile:Vector2i) -> int:
+## Used to evaluate where the path tiles are around the given file. Max value
+## is 15.
+## Score +1 if path tile above (y-1)
+## Score +2 if path tile to the right (x+1)
+## Score +4 if path tile below (y+1)
+## Score +8 if path tile to the left (x-1)
+func get_tile_score(index:int) -> int:
 	var score:int = 0
-	var x = tile.x
-	var y = tile.y
+	var x = _path[index].x
+	var y = _path[index].y
 	
 	score += 1 if _path.has(Vector2i(x,y-1)) else 0
 	score += 2 if _path.has(Vector2i(x+1,y)) else 0
@@ -46,3 +61,95 @@ func get_tile_score(tile:Vector2i) -> int:
 
 func get_path() -> Array[Vector2i]:
 	return _path
+
+func _add_loops():
+	# See if we can add any loops
+	for i in range(_path.size()):
+		var loop:Array[Vector2i] = _is_loop_option(i)
+		if loop.size() > 0:
+			for j in range(loop.size()):
+				_path.insert(i+1+j, loop[j])
+
+## For a given index in the path, evaluate whether a loop can be generated
+## around it.
+func _is_loop_option(index:int) -> Array[Vector2i]:
+	var x: int = _path[index].x
+	var y: int = _path[index].y
+	var return_path:Array[Vector2i]
+
+	#Yellow
+	if (x < _grid_length-1 and y > 1
+		and _tile_loc_free(x, y-3) and _tile_loc_free(x+1, y-3) and _tile_loc_free(x+2, y-3)		
+		and _tile_loc_free(x-1, y-2) and _tile_loc_free(x, y-2) and _tile_loc_free(x+1, y-2) and _tile_loc_free(x+2, y-2) and _tile_loc_free(x+3, y-2)
+		and _tile_loc_free(x-1, y-1) and _tile_loc_free(x, y-1) and _tile_loc_free(x+1, y-1) and _tile_loc_free(x+2, y-1) and _tile_loc_free(x+3, y-1)
+		and _tile_loc_free(x+1,y) and _tile_loc_free(x+2,y) and _tile_loc_free(x+3,y)
+		and _tile_loc_free(x+1,y+1) and _tile_loc_free(x+2,y+1)):
+		return_path = [Vector2i(x+1,y), Vector2i(x+2,y), Vector2i(x+2,y-1), Vector2i(x+2,y-2), Vector2i(x+1,y-2), Vector2i(x,y-2), Vector2i(x,y-1)]
+
+		if _path[index-1].y > y:
+			return_path.reverse()
+			
+		_loop_count += 1
+#		print("Yellow")
+		return_path.append(Vector2i(x,y))
+	#Blue
+	elif (x > 2 and y > 1
+			and _tile_loc_free(x, y-3) and _tile_loc_free(x-1, y-3) and _tile_loc_free(x-2, y-3)		
+			and _tile_loc_free(x-1, y) and _tile_loc_free(x-2, y) and _tile_loc_free(x-3, y)
+			and _tile_loc_free(x+1, y-1) and _tile_loc_free(x, y-1) and _tile_loc_free(x-2, y-1) and _tile_loc_free(x-3, y-1)
+			and _tile_loc_free(x+1, y-2) and _tile_loc_free(x, y-2) and _tile_loc_free(x-1, y-2) and _tile_loc_free(x-2, y-2) and _tile_loc_free(x-3, y-2)
+			and _tile_loc_free(x-1, y+1) and _tile_loc_free(x-2, y+1)):
+		return_path = [Vector2i(x,y-1), Vector2i(x,y-2), Vector2i(x-1,y-2), Vector2i(x-2,y-2), Vector2i(x-2,y-1), Vector2i(x-2,y), Vector2i(x-1,y)]
+		
+		if _path[index-1].x > x:
+			return_path.reverse()
+
+		_loop_count += 1
+#		print("Blue")
+		return_path.append(Vector2i(x,y))
+	#Red
+	elif (x < _grid_length-1 and y < _grid_height-2
+			and _tile_loc_free(x, y+3) and _tile_loc_free(x+1, y+3) and _tile_loc_free(x+2, y+3)		
+			and _tile_loc_free(x+1, y-1) and _tile_loc_free(x+2, y-1)
+			and _tile_loc_free(x+1, y) and _tile_loc_free(x+2, y) and _tile_loc_free(x+3, y)
+			and _tile_loc_free(x-1, y+1) and _tile_loc_free(x, y+1) and _tile_loc_free(x+2, y+1) and _tile_loc_free(x+3, y+1)
+			and _tile_loc_free(x-1, y+2) and _tile_loc_free(x, y+2) and _tile_loc_free(x+1, y+2) and _tile_loc_free(x+2, y+2) and _tile_loc_free(x+3, y+2)):
+		return_path = [Vector2i(x+1,y), Vector2i(x+2,y), Vector2i(x+2,y+1), Vector2i(x+2,y+2), Vector2i(x+1,y+2), Vector2i(x,y+2), Vector2i(x,y+1)]
+
+		if _path[index-1].y < y:
+			return_path.reverse()
+		
+		_loop_count += 1
+#		print("Red")
+		return_path.append(Vector2i(x,y))
+	# Brown
+	elif (x > 2 and y < _grid_height-2
+			and _tile_loc_free(x, y+3) and _tile_loc_free(x-1, y+3) and _tile_loc_free(x-2, y+3)
+			and _tile_loc_free(x-1, y-1) and _tile_loc_free(x-2, y-1)
+			and _tile_loc_free(x-1, y) and _tile_loc_free(x-2, y) and _tile_loc_free(x-3, y)
+			and _tile_loc_free(x+1, y+1) and _tile_loc_free(x, y+1) and _tile_loc_free(x-2, y+1) and _tile_loc_free(x-3, y+1)
+			and _tile_loc_free(x+1, y+2) and _tile_loc_free(x, y+2) and _tile_loc_free(x-1, y+2) and _tile_loc_free(x-2, y+2) and _tile_loc_free(x-3, y+2)):
+		return_path = [Vector2i(x,y+1	), Vector2i(x,y+2), Vector2i(x-1,y+2), Vector2i(x-2,y+2), Vector2i(x-2,y+1), Vector2i(x-2,y), Vector2i(x-1,y)]
+
+		if _path[index-1].x > x:
+			return_path.reverse()
+		
+		_loop_count += 1
+#		print("Brown")
+		return_path.append(Vector2i(x,y))
+		
+	return return_path
+	
+## Returns true if there is a path tile at the x,y coordinate
+func _tile_loc_taken(x: int, y: int) -> bool:
+	return _path.has(Vector2i(x,y))
+	
+## Returns true if there is no path tile at the x,y coordinate
+func _tile_loc_free(x: int, y: int) -> bool:
+	return not _path.has(Vector2i(x,y))
+	
+func get_loop_count() -> int:
+	return _loop_count
+	
+func get_path_tile(index:int) -> Vector2i:
+	return _path[index]
